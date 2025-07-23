1656647df80d31742e5bf8268aa15396556d6de5
Yuri Santos <yuri.rps@gmail.com>
2025-07-23 16:18:12 -0300

    setup inicial e tipografia

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'dogsapi.origamid.dev'
+    }]
+  }
 };
 
 export default nextConfig;
