f7756f82a7f519bdafd54b9c15bcaa7e57c8d770
Yuri Santos <yuri.rps@gmail.com>
2025-08-09 23:55:59 -0300

    fix: Update useFormState to useActionState

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import login from "@/actions/login";
-import { useFormState, useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 import Button from "@/components/forms/button";
+import { useActionState } from "react";
 
 function FormButton() {
   const { pending } = useFormStatus();
@@ -19,12 +20,11 @@ function FormButton() {
 }
 
 export default function LoginForm() {
-
-const [state, action] =useFormState(login, {
-  ok: false,
-  error: '',
-  data: null
-})
+  const [state, action] = useActionState(login, {
+    ok: false,
+    error: "",
+    data: null,
+  });
 
   return (
     <>
