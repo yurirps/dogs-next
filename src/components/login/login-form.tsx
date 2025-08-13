01d4cab64916e0fe385078b67f73ce850ad2575b
Yuri Santos <yuri.rps@gmail.com>
2025-08-13 00:20:21 -0300

    feat: input e estilização login

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -3,7 +3,11 @@
 import login from "@/actions/login";
 import { useFormStatus } from "react-dom";
 import Button from "@/components/forms/button";
-import { useActionState } from "react";
+import React, { useActionState } from "react";
+import Input from "@/components/forms/input";
+import ErrorMessage from "../helper/error-message";
+import Link from "next/link";
+import styles from "./login-form.module.css";
 
 function FormButton() {
   const { pending } = useFormStatus();
@@ -26,14 +30,27 @@ export default function LoginForm() {
     data: null,
   });
 
+  //redirecionar se fizer login
+  React.useEffect(() => {
+    if (state.ok) window.location.href = "/conta";
+  }, [state.ok]);
+
   return (
     <>
-      <form action={action}>
-        <input type="text" name="username" placeholder="usuário" />
-        <input type="password" name="password" placeholder="senha" />
+      <form action={action} className={styles.form}>
+        <Input label="Usuário" name="username" type="text" />
+        <Input label="Senha" name="password" type="password" />
+        <ErrorMessage error={state.error} />
         <FormButton />
-        <p>{state.error}</p>
       </form>
+      <Link href="/login/perdeu" className={styles.perdeu}>
+        Esqueceu sua senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Crie sua conta agora mesmo!</p>
+        <Link href="/login/criar" className="button" >Criar Conta</Link>
+      </div>
     </>
   );
 }
