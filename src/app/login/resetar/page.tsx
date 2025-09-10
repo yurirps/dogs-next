2c368c1c3fa69b360f8ca6904fc3e1dbc1500a95
Yuri Santos <yuri.rps@gmail.com>
2025-09-09 23:42:47 -0300

    feat: Resetar senha

--- src/app/login/resetar/page.tsx
+++ src/app/login/resetar/page.tsx
@@ -1,15 +1,24 @@
-import { Metadata } from "next"
+import LoginResetarForm from "@/components/login/login-resetar-form";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'Resetar senha | Dogs',
-    description: 'Resetar senha'
+  title: "Resetar senha | Dogs",
+  description: "Resetar senha",
+};
+
+type ResetarSearchParams = {
+    searchParams: {
+        key: string,
+        login: string
+    }
 }
 
-
-export default async function ResetarPage() {
-    return (
-        <main>
-            <h1>Resetar</h1>
-        </main>
-    )
+export default async function ResetarPage({searchParams}: ResetarSearchParams) {
+    console.log(searchParams)
+  return (
+    <div className="animeLeft">
+      <h1 className="title">Nova Senha</h1>
+      <LoginResetarForm keyToken={searchParams.key} login={searchParams.login} />
+    </div>
+  );
 }
