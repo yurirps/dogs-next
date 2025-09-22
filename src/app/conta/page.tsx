ddbad735431f0fc90b9d4e904970ee0ada8a01bd
Yuri Santos <yuri.rps@gmail.com>
2025-09-22 00:08:26 -0300

    feat: Adição de context

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,7 +1,14 @@
-export default async function ContaPage() {
-    return (
-        <main>
-            <h1>Conta</h1>
-        </main>
-    )
+"use client";
+
+import { useUser } from "@/context/user-context";
+import React from "react";
+
+export default function ContaPage() {
+  const { user } = useUser();
+
+  return (
+    <main>
+      <h1>Conta: {user?.nome}</h1>
+    </main>
+  );
 }
