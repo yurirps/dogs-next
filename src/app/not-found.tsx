b460cbdb886e6e887eabb311930016b4ebb90ce5
Yuri Santos <yuri.rps@gmail.com>
2025-10-14 12:56:15 -0300

    feat: Paginas de erro e notFound

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <section
+      className="container"
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1 className="tittle" style={{ marginTop: "1.5rem" }}>
+        Página não encontrada
+      </h1>
+      <Link
+        className="button"
+        style={{ marginBottom: "1rem", display: "inline-block" }}
+        href={"/"}
+      >
+        {" "}
+        Volte para home{" "}
+      </Link>
+    </section>
+  );
+}
